7a8ad172b4363892ce18640d94cf947bee4e921d
aliegha <elijerroberts@gmail.com>
2017-01-17 06:34:24 -0700

    Update nagiosbot.py

--- nagiosbot.py
+++ nagiosbot.py
@@ -26,11 +26,11 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import re
+#import re
 
 import errbot
 import nagcgi
-import rt
+#import rt
 
 class NagiosBot(errbot.BotPlugin):
 	min_err_version = "2.0.0"
